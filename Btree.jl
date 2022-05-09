07ce96639e046fc742ea21c798297a38de79863e
Eric Zhang <eric.ca.zhang@gmail.com>
2022-05-09 18:40:56 -0400

    Add files via upload

--- Btree.jl
+++ Btree.jl
@@ -184,3 +184,209 @@ end
 function lower_bound(tree::Tree{K}, k::K) where K
     return lower_bound(tree.root, k)
 end
+
+@propagate_inbounds function insert!(node::Node{K}, iData::K)::Union{Missing, Tuple{K, Node{K}}}  where K
+    i = fgeq(node, iData)
+
+    if i <= node.numData && node.data[i] == iData
+        node.data[i] = iData
+        return missing
+    end
+    
+    if !isLeaf(node)
+        ret = insert!(node.children[i], iData)
+        
+        if ismissing(ret)
+            return missing
+        end
+        
+        iData, iChild = ret
+    end
+    
+
+    split = missing
+    if node.numData == S
+        rNode = Node{K}()
+        m = S ÷ 2 + 1 # bias left
+
+        if S & 1 == 0 # adjustments for even case
+            if i == m # median                
+                copy!(rNode.data, 1, node.data, m, node.numData)
+                
+                if !isLeaf(node)
+                    copy!(rNode.children, 2, node.children, m+1, node.numData + 1)
+                    rNode.children[1] = iChild
+                end
+                
+                rNode.numData = node.numData - (m - 1)
+                
+                node.numData = m - 1
+                
+                return iData, rNode
+            elseif i < m # if biased and left
+                m -= 1
+            end
+        end
+        
+        copy!(rNode.data, 1, node.data, m+1, node.numData)
+        
+        if !isLeaf(node)
+            copy!(rNode.children, 1, node.children, m+1, node.numData + 1)
+        end
+        
+        rNode.numData = node.numData - m
+        node.numData = m-1
+        split = node.data[m]
+        
+        if i > m
+            i -= m
+            node = rNode
+        end
+    end
+
+    shiftr!(node.data, i, node.numData)
+    node.data[i] = iData
+    
+    # insert!(node.data, i, iData)
+    
+    # println(node.numData)
+    if !isLeaf(node)
+        shiftr!(node.children, i+1, node.numData + 1)
+        node.children[i+1] = iChild
+    end
+    
+    node.numData += 1
+        
+    return ismissing(split) ? missing : (split, rNode)
+end
+
+function insert!(tree::Tree{K}, iData::K) where K
+    ret = insert!(tree.root, iData)
+    
+    if !ismissing(ret)
+        nRoot = Node{K}()
+        nRoot.children[1] = tree.root
+        nRoot.data[1], nRoot.children[2] = ret
+        nRoot.numData = 1
+        tree.root = nRoot
+    end
+end
+
+function push!(tree::Tree{K}, iData::K) where K
+    insert!(tree, iData)
+end
+
+@propagate_inbounds function delete!(node::Node{K}, par::OptNode, pNodeI::Int, dData::K)::Union{Missing, Int, Node{K}} where K
+    i = fgeq(node, dData)
+    
+    if !isLeaf(node)
+        if i <= node.numData && node.data[i] == dData
+            dNode = node.children[i]
+
+            while !isLeaf(dNode)
+                dNode = dNode.children[dNode.numData + 1]
+            end
+            rValue = dNode.data[dNode.numData]
+
+            node.data[i] = rValue
+
+            dData = rValue
+        end
+        
+        i = delete!(node.children[i], node, i, dData)
+        if ismissing(i)
+            return missing
+        end
+    end
+    
+    if isLeaf(node)
+        if i > node.numData || node.data[i] != dData
+            return missing
+        end
+    end
+    
+    # by this point, assumes data i and children i+1 can be safely removed
+
+    shiftl!(node.data, i+1, node.numData)
+    if !isLeaf(node)
+        shiftl!(node.children, i+2, node.numData+1)
+    end
+    
+    node.numData -= 1
+    
+    if node.numData < S / 2
+        if pNodeI == -1
+            # root case: can't do anything about it
+            
+            if node.numData == 0
+                return node.children[1]
+            end
+            
+            return missing
+        end
+        
+        if pNodeI != 1
+            lSib = par.children[pNodeI-1]
+            if lSib.numData > S / 2
+                shiftr!(node.data, 1, node.numData)
+                
+                if !isLeaf(node)
+                    shiftr!(node.children, 1, node.numData+1)
+                    node.children[1] = lSib.children[lSib.numData+1]
+                    lSib.children[lSib.numData+1] = missing
+                end
+                
+                node.data[1] = par.data[pNodeI - 1]
+                par.data[pNodeI - 1] = lSib.data[lSib.numData]
+                
+                node.numData += 1
+                lSib.numData -= 1
+                return missing
+            end
+        end
+            
+        if pNodeI != par.numData + 1
+            rSib = par.children[pNodeI+1]
+            if rSib.numData > S / 2
+                node.data[node.numData+1] = par.data[pNodeI]
+                par.data[pNodeI] = rSib.data[1]
+                shiftl!(rSib.data, 2, rSib.numData)
+                
+                if !isLeaf(node)
+                    node.children[node.numData+2]= rSib.children[1]
+                    shiftl!(rSib.children, 2, rSib.numData+1)
+                end
+                
+                node.numData += 1
+                rSib.numData -= 1
+                return missing
+            end
+        end
+        
+        if pNodeI != 1
+            rNode = node
+            node = par.children[pNodeI-1]
+            pNodeI -= 1
+        else
+            rNode = par.children[pNodeI+1]
+        end
+        node.data[node.numData + 1] = par.data[pNodeI]
+
+        copy!(node.data, node.numData + 2, rNode.data, 1, rNode.numData)
+        
+        copy!(node.children, node.numData + 2, rNode.children, 1, rNode.numData+1)
+        
+        node.numData += 1 + rNode.numData
+                
+        return pNodeI
+    end
+    return missing
+end
+
+function delete!(tree::Tree{K}, dData::K) where K
+    ret = delete!(tree.root, missing, -1, dData)
+    if !ismissing(ret)
+        tree.root = ret
+    end
+    return nothing
+end
